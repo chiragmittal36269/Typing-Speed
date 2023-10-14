8731d1021888d32c1abeea961f2ba273c6d35e32
Chirag Mittal <chiragmittal36269@gmail.com>
2023-10-15 01:27:06 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
+import TypingInput from "./Components/TypingInput";
 
 const App = () => {
     return (
         <div>
-            {/* <TypingInput /> */}
-            <h1>plug into nirvana</h1>
+            <TypingInput />
         </div>
     );
 };
