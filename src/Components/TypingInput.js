bd86d18410242ba194700945d7d2f5d3ad0c5aa2
Chirag Mittal <chiragmittal36269@gmail.com>
2023-10-15 01:04:05 +0530

    updated css

--- src/Components/TypingInput.js
+++ src/Components/TypingInput.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 
 const TypingInput = () => {
-    const [userInput, setUerInput] = useState("");
+    const [userInput, setUerInput] = useState(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    );
 
     return (
         <div>
@@ -53,8 +55,6 @@ const TypingInput = () => {
                         <textarea
                             name=""
                             id=""
-                            cols="30"
-                            rows="10"
                             value={userInput}
                             onChange={(e) =>
                                 setUerInput(e.target.value)
@@ -62,14 +62,16 @@ const TypingInput = () => {
                     </div>
 
                     <div className="refresh">
-                        <svg
-                            className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
-                            focusable="false"
-                            aria-hidden="true"
-                            viewBox="0 0 24 24"
-                            data-testid="RefreshIcon">
-                            <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"></path>
-                        </svg>
+                        <button>
+                            <svg
+                                className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
+                                focusable="false"
+                                aria-hidden="true"
+                                viewBox="0 0 24 24"
+                                data-testid="RefreshIcon">
+                                <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"></path>
+                            </svg>
+                        </button>
                     </div>
 
                     <div className="reset">
@@ -91,13 +93,53 @@ const TypingInput = () => {
 
                     <div className="footer">
                         <div className="social">
-                            <div className="github"></div>
-                            <div className="linkedIn"></div>
-                            <div className="mail"></div>
-                            <div className="instagram"></div>
+                            <a href="/" className="github">
+                                <svg
+                                    class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
+                                    focusable="false"
+                                    aria-hidden="true"
+                                    viewBox="0 0 24 24"
+                                    data-testid="GitHubIcon">
+                                    <path d="M12 1.27a11 11 0 00-3.48 21.46c.55.09.73-.28.73-.55v-1.84c-3.03.64-3.67-1.46-3.67-1.46-.55-1.29-1.28-1.65-1.28-1.65-.92-.65.1-.65.1-.65 1.1 0 1.73 1.1 1.73 1.1.92 1.65 2.57 1.2 3.21.92a2 2 0 01.64-1.47c-2.47-.27-5.04-1.19-5.04-5.5 0-1.1.46-2.1 1.2-2.84a3.76 3.76 0 010-2.93s.91-.28 3.11 1.1c1.8-.49 3.7-.49 5.5 0 2.1-1.38 3.02-1.1 3.02-1.1a3.76 3.76 0 010 2.93c.83.74 1.2 1.74 1.2 2.94 0 4.21-2.57 5.13-5.04 5.4.45.37.82.92.82 2.02v3.03c0 .27.1.64.73.55A11 11 0 0012 1.27"></path>
+                                </svg>
+                            </a>
+
+                            <a href="/" className="LinkedIn">
+                                <svg
+                                    class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
+                                    focusable="false"
+                                    aria-hidden="true"
+                                    viewBox="0 0 24 24"
+                                    data-testid="LinkedInIcon">
+                                    <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"></path>
+                                </svg>
+                            </a>
+
+                            <a href="/" className="mail">
+                                <svg
+                                    class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
+                                    focusable="false"
+                                    aria-hidden="true"
+                                    viewBox="0 0 24 24"
+                                    data-testid="AttachEmailIcon">
+                                    <path d="M21 10V4c0-1.1-.9-2-2-2H3c-1.1 0-1.99.9-1.99 2L1 16c0 1.1.9 2 2 2h11v-5c0-1.66 1.34-3 3-3h4zm-10 1L3 6V4l8 5 8-5v2l-8 5z"></path>
+                                    <path d="M21 14v4c0 1.1-.9 2-2 2s-2-.9-2-2v-4.5c0-.28.22-.5.5-.5s.5.22.5.5V18h2v-4.5c0-1.38-1.12-2.5-2.5-2.5S15 12.12 15 13.5V18c0 2.21 1.79 4 4 4s4-1.79 4-4v-4h-2z"></path>
+                                </svg>
+                            </a>
+
+                            <a href="/" className="Instagram">
+                                <svg
+                                    class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
+                                    focusable="false"
+                                    aria-hidden="true"
+                                    viewBox="0 0 24 24"
+                                    data-testid="InstagramIcon">
+                                    <path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path>
+                                </svg>
+                            </a>
                         </div>
                         <div className="themeMode">
-                            <select name="" id="">
+                            <select name="" id="themes">
                                 <option value="">Darken-Black</option>
                                 <option value="">Coloured-Grey</option>
                                 <option value="">Coloured-Pink</option>
